daa09ee36ccc899fde8fb512f6663afcacd2ac5c
bilalbash <bilal.basharat@gmail.com>
2020-09-15 22:03:26 +0500

    added jquery webpacker popper.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquert/src/jquery',
+    jQuery: 'jquert/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
