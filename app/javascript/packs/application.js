71d7fe97cd448e0c05bd3fce52ecc67c2507874c
bilalbash <bilal.basharat@gmail.com>
2020-09-17 22:04:55 +0500

    fixed bootstrap, turbolinks, jquery and css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+import $ from 'jquery';
+// import Popper from 'popper.js';
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap")
 
-requrie("bootstrap")
+document.addEventListener('turbolinks:load', () => {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+  console.log('all seems well');
+})
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
